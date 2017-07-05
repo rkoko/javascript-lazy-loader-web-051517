2e0ceba349bb26f5720cf2dae251fd7cbf0aab94
rkoko <rinda.ko@gmail.com>
2017-07-05 12:21:13 -0400

    Done.

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,6 +1,9 @@
 "use strict";
 
 $(document).ready(function() {
+  $('#load-cars').on('click', function(){
+    fetchJSON()
+  })
   // add click listener here
   // it should call on fetchJSON()
 });
